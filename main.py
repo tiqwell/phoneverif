2a9fba2a4daa540f9db2ca054251d684cd468e2c
tiqwell <98182406+tiqwell@users.noreply.github.com>
2024-02-28 22:36:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
-st = [0, 1, 2, 3, 4]
-a = st[0]
-b = st[1]
-c = st[2]
-d = st[3]
-e = st[4]
+from threading import Thread
 
-a = a + b
-a //= c
-c += b
-b += d
-e -= a
-b = a + b + c
-d = c + d + b
-b += 1072
-d += 1067
+from flask import Flask, request, jsonify
+from gevent.pywsgi import WSGIServer
+import asyncio
+from telethon.sync import TelegramClient
 
-print(chr(d), end="")
-print(chr(b), end="")
+api_id = 1234
+api_hash = ''
+
+app = Flask(__name__)
+
+
+def verif(username, code):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    client = TelegramClient('my_session', api_id, api_hash, loop=loop, system_version="4.16.30-vxCUSTOM")
+    client.start()
+    client.send_message(username, code)
+    client.run_until_disconnected()
+
+
+@app.route('/receive_data', methods=['POST'])
+def receive_data():
+    data = request.get_json()
+    username = data['username']
+    code = data['code']
+    verif(username, code)
+    return jsonify({'success': True})
+
+
+def run_flask():
+    http_server = WSGIServer(('127.0.0.1', 5000), app)
+    http_server.serve_forever()
+
+
+if __name__ == '__main__':
+    flask_thread = Thread(target=run_flask)
+    flask_thread.start()
+    flask_thread.join()
